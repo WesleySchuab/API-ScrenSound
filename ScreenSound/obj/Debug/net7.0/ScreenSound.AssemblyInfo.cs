ee9b4712ce2e2a3ad4cde7918bbe2770e5a1fade
Wesley Schuab <yinnihisoka@gmail.com>
2024-04-08 08:17:15 -0300

    Adcionando a referência ScreensoundModelos em ScreenSoundShared.Dados

--- ScreenSound/obj/Debug/net7.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/obj/Debug/net7.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21aaf2284e504d6f931594912b82b09eabf8026a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ec336daf82369998b92a5baa990996995bb696")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
